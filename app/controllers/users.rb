c32f216c4abec827f2b09dd7c79c4b3f3a135d2c
nextacademy <nextacademy@email.com>
2015-10-14 23:21:31 +0800

    CRUD user

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,9 +1,37 @@
-# post '/users/new' do
+require 'byebug'
+
+get '/users/new' do
 	
-# 	erb :"static/signup"
-# end
+	erb :"users/new"
+end
+
+post '/users/create' do
+	@user = User.create(username: params[:username],password: params[:password])
+	erb :"users/show"
+
+end
+
+get '/users/:id' do
+	@user = User.find(params[:id])
+	erb :"users/show"
+end
+
+get '/users/:id/edit' do#edit
+	@user = User.find(params[:id])
+	erb :"users/edit"
+end
+
+post '/users/:id' do#update
+	@user = User.find(params[:id])
+	if params[:decision] == "Edit"
+		@user.update(username: params[:username], password: params[:password])
+		erb :"users/show"
+	elsif params[:decision] == "Delete"
+		@user.destroy
+		erb :"static/index"
+	end
+end
 
 
-# # show
 
 
